d889a36005bf3033ea8cb759645ec9ec51328abe
Balthazar16 <mattlmorgan@sbcglobal.net>
2016-11-09 19:18:35 -0500

    Update FoilMakerModel.java

--- Project3/src/FoilMakerModel.java
+++ Project3/src/FoilMakerModel.java
@@ -4,37 +4,45 @@
  * Created by Ian on 10/27/2016.
  */
 public class FoilMakerModel {
-    public static FoilMakerNetworkProtocol.MSG_TYPE lastSentMessageType;
-    public static FoilMakerNetworkProtocol.MSG_TYPE lastReceivedMessageType;
-    public static FoilMakerNetworkProtocol.MSG_DETAIL_T lastReceivedMessageDetail;
+	public FoilMakerController controller;
+	
+    public  FoilMakerNetworkProtocol.MSG_TYPE lastSentMessageType;
+    public  FoilMakerNetworkProtocol.MSG_TYPE lastReceivedMessageType;
+    public  FoilMakerNetworkProtocol.MSG_DETAIL_T lastReceivedMessageDetail;
 
-    public static String username;
-    public static String password;
-    public static String userToken;
-    public static String gameToken;
+    public  String username;
+    public  String password;
+    public  String userToken;
+    public  String gameToken;
 
-    public static String currentQuestion;
-    public static String currentCorrectAnswer;
+    public  String currentQuestion;
+    public  String currentCorrectAnswer;
 
-    public static String player2Username;
-    public static int player2Score;
+    public  String player2Username;
+    public  int player2Score;
 
-    public static int currentNumberOfPlayers = 1;
-    public static final int MAX_NUMBER_PLAYERS = 2;
+    public  int currentNumberOfPlayers = 1;
+    public  final int MAX_NUMBER_PLAYERS = 2;
 
-    public static String possibleAnswer1;
-    public static String possibleAnswer2;
-    public static String possibleAnswer3;
+    public  String possibleAnswer1;
+    public  String possibleAnswer2;
+    public  String possibleAnswer3;
+    public  String chosenAnswer;
 
-    public static String hostRoundResultMessage;
-    public static int hostCumulativeScore;
-    public static int numPlayersFooledByHost;
-    public static int numTimesHostFooledByOthers;
+    public  String hostRoundResultMessage;
+    public  int hostCumulativeScore;
+    public  int numPlayersFooledByHost;
+    public  int numTimesHostFooledByOthers;
 
-    public static String player2RoundResultMessage;
-    public static int player2CumulativeScore;
-    public static int numPlayersFooledByPlayer2;
-    public static int numTimesPlayer2FooledByOthers;
+    public  String player2RoundResultMessage;
+    public  int player2CumulativeScore;
+    public  int numPlayersFooledByPlayer2;
+    public  int numTimesPlayer2FooledByOthers;
 
-    public static boolean isGameOver = false;
+    public  boolean isGameOver = false;
+    public boolean yupHost;
+    
+    public FoilMakerModel(FoilMakerController c){
+    	this.controller = c;
+    }
 }
